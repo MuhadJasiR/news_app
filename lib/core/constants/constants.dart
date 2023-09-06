a34a9d5ea0930da2a8dc408b6511741919c0b9c9
MuhadJasiR <alijasir051@gmail.com>
2023-09-06 18:36:10 +0530

    fixed retrofit error

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,4 @@
+const String newsApiBaseUrl = "https://newsapi.org/v2";
+const String newsAPIKey = "78bc5492788946af86c78cd7b864207e";
+const String countryQuery = "us";
+const String categoryQuery = "general";
