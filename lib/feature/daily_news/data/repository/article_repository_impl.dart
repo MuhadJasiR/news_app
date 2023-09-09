fc40055fb3e068ec11028d40c2b5b7ef578e882f
MuhadJasiR <alijasir051@gmail.com>
2023-09-09 15:58:15 +0530

    got fixed the retrofit error

--- lib/feature/daily_news/data/repository/article_repository_impl.dart
+++ lib/feature/daily_news/data/repository/article_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:news_app/core/resources/data_state.dart';
 import 'package:news_app/feature/daily_news/data/data_sources/remote/news_api_service.dart';
 import 'package:news_app/feature/daily_news/domain/entities/article.dart';
@@ -6,18 +7,20 @@ import 'package:news_app/feature/daily_news/domain/respository/article_resposito
 import '../../../../core/constants/constants.dart';
 
 class ArticleRepositoryImpl implements ArticleRepository {
-  final NewsApiServices _newsApiServices;
+  final NewsApiService _newsApiServices;
   ArticleRepositoryImpl(this._newsApiServices);
 
-  // @override
-  // Future<DataState<List<ArticleEntity>>> getNewsArticle() async {
-  //   // final httpResponse = await _newsApiServices.getNewsArticles(
-  //   //   apikey: newsAPIKey,
-  //   //   country: countryQuery,
-  //   //   category: categoryQuery,
-  //   // );
-  //   // if (httpResponse) {
-
-  //   // }
-  // }
+  @override
+  Future<DataState<List<ArticleEntity>>> getNewsArticle() async {
+    try {
+      final articles = await _newsApiServices.getNewsArticles(
+        apiKey: newsAPIKey,
+        country: countryQuery,
+        category: categoryQuery,
+      );
+      return DataSuccess(articles);
+    } on DioException catch (e) {
+      return DataFailed(e);
+    }
+  }
 }
